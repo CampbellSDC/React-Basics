62e882f5d80ceda07e42eea0e5813ef845a05adc
Steve <scampbellatc@gmail.com>
2023-06-26 06:34:30 -0400

    updated CSS to page

--- index.js
+++ index.js
@@ -3,13 +3,16 @@
 function HeaderEl() {
     return (
         <header>
-        <img src='/React_logo.png'></img>
-        <ul>
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-    </header>
+            <nav className="navbar">
+                <img className="react-logo" src='/React_logo.png'></img>
+                
+                <ul className="nav-list">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
     )
 
     Main()
@@ -19,9 +22,9 @@ function HeaderEl() {
 
 function Main(){
     return (
-        <div>
+        <div className="main">
             <h1>Reasons I'm Excited to Learn React</h1>
-            <ul>
+            <ul className="main-list">
                 <li>It's a popular library</li>
                 <li>Increasing skill set to build more reusable code</li>
                 <li>More likely to get a job as a developer with this knowledge</li>
@@ -34,7 +37,7 @@ function Main(){
 
 function Footer() {
     return (
-        <footer>
+        <footer className="footer">
             <small>© 2021 Campbell development. All rights reserved.</small>
         </footer>
     )
